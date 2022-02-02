82b5ce8be36aedd0e3eb677eea67129233a25cea
Kmilo Denis Glez <kmilo.denis.glez@gmail.com>
2022-02-02 16:45:48 -0500

    updated fabric images version to 2.2.5

--- network.sh
+++ network.sh
@@ -171,8 +171,8 @@ CC_VERSION="1.0.0"
 CC_SEQUENCE=1
 # default image tag
 # IMAGETAG="latest"
-# IMAGETAG="2.2.5"
-IMAGETAG="2.2.0"
+IMAGETAG="2.2.5"
+#IMAGETAG="2.2.0"
 # default ca image tag
 # CA_IMAGETAG="latest"
 #CA_IMAGETAG="1.5.2"
